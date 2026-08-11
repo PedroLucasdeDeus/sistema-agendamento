696237b4966e5a672003f1e13c3dcae16ebde945
MonnikLuianne <monnikgithubdev@gmail.com>
2026-08-11 16:44:30 -0300

    Atualizar o app.py
    melhora pouca

--- app.py
+++ app.py
@@ -11,16 +11,12 @@ def create_app():
 
     app = Flask(__name__)
 
-    # Carrega configurações
     app.config.from_object(Config)
 
-    # Inicializa banco
     db.init_app(app)
 
-    # Registra rotas de clientes
     app.register_blueprint(clientes_bp)
 
-    # Cria banco/tabelas
     init_db(app)
 
     return app
